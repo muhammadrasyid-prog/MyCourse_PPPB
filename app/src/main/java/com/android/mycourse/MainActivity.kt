ffd684ac0cb34a921ec043a780411b73dec1c889
muhammadrasyid-prog <muhammadrasyid712@gmail.com>
2026-09-24 09:30:21 +0700

    force close

--- app/src/main/java/com/android/mycourse/MainActivity.kt
+++ app/src/main/java/com/android/mycourse/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.android.mycourse
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.android.mycourse.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionPagerAdapter(this)
+
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+
+        viewPager.adapter = sectionsPagerAdapter
+
+        val tabs: TabLayout = findViewById(R.id.tab_layout)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+        supportActionBar?.elevation = 0f
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_options, menu)
         return true
---
+++ app/src/main/java/com/android/mycourse/SectionPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.android.mycourse
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = MateriFragment()
+            2 -> fragment = QuizFragment()
+        }
+        return fragment as Fragment
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+}
